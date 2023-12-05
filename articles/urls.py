97be66e06cf2d147ee5a9bc1418a4fd2c5e02c86
Sufferman <67066216+sufferman@users.noreply.github.com>
2023-12-05 11:56:00 +0330

    url

--- articles/urls.py
+++ articles/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 
-app_name = 'articles'
+app_name = "articles"
 urlpatterns = [
-    path('', views.articles_list, name='list'),
-    path('<slug>', views.article_detail),
+    path('', views.articles_list, name="list"),
+    path('<slug>', views.article_detail, name="detail"),
 
 ]
