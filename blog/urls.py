e8b7ed71ab9a04b926cce3d181dd6dee67e6fc46
Sufferman <67066216+sufferman@users.noreply.github.com>
2023-12-07 13:22:01 +0330

    Media and ax

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home),
